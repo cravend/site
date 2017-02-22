d3905edbdbd19af968b424d98a0203b992edb6a4
Dalton Craven <20dalton00@gmail.com>
2017-02-22 16:46:32 +0000

    Added keybinding to that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -4,3 +4,11 @@ function sayThatWasEasy() {
 }
 
 $("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+    if (event.charCode == 32) {
+        $("#easy").trigger("click");
+    }
+}
