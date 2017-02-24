ed56a947cfe6ca922b2766d48dc450105ff23e12
Dalton Craven <20dalton00@gmail.com>
2017-02-24 14:54:41 +0000

    Shifted Circles

--- geometric_pattern/app.js
+++ geometric_pattern/app.js
@@ -1,18 +1,30 @@
 var numCircles = 12;
 var circleDiameter;
+var circleRadius;
 function setup() {
     createCanvas(480, 600);
-    circleDiameter = width/numCircles
+    circleDiameter = width/numCircles;
+    circleRadius = circleDiameter/2;
 }
 
 function draw() {
+    var isShifted = false;
     var y = 0;
+    
     while (y <= height) {
-        var x = 0;
+        var x;
+        if (isShifted) {
+            x = circleRadius;
+        } else {    
+            x = 0;
+        }
+        
         while (x <= width) {
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
-        y = y + circleDiameter;
+        
+        y = y + circleRadius;
+        isShifted = !isShifted;
     }
 }
