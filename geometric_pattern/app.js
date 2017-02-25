da8ec2a3e03554818f49d5d4d339816a900e0624
Dalton Craven <20dalton00@gmail.com>
2017-02-25 02:37:37 +0000

    added bigfoot project, consolidated menu into nav.html, added js to push menus to each page

--- geometric_pattern/app.js
+++ geometric_pattern/app.js
@@ -10,29 +10,31 @@ function setup() {
 function draw() {
     var isShifted = false;
     var y = height;
-    var rVal = 255;
-    var gVal = 0;
-    var bVal = 0;
+    var rVal = 84;
+    var gVal = 21;
+    var bVal = 16;
+    
+    // 228  ->  180 +6      156,21,16
+    // 45  -> 21 +1
+    // 64  -> 16 -2 
+    
     
     while (y >= 0) {
         var x;
-        if (isShifted) {
-            x = circleRadius;
-        } else {    
-            x = 0;
-        }
+        if (isShifted) {x = circleRadius;} else {x = 0;}
         
         while (x <= width) {
             fill(color(rVal, gVal, bVal));
-            stroke(color(rVal, gVal, bVal));
+            // stroke(color(rVal, gVal, bVal));
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
-        
+        // 24 circles
         y = y - circleRadius;
         isShifted = !isShifted;
-        rVal = rVal - 2;
-        gVal = gVal + 7;
-        bVal = bVal + 3;
+        rVal = rVal + 12;
+        gVal = gVal + 2;
+        bVal = bVal + 4;
+        
     }
 }
