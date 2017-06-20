bd195a7895719011e50b6f5983a51ea3a4b2c395
Dalton Craven <20dalton00@gmail.com>
2017-06-20 02:11:00 +0000

    add cool button effects

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $("#moreprojects").on("hide.bs.collapse", function(){
-    $("#toggleproject").html('<h4>See More</h4>');
+    $("#toggleproject").html('<button class="btn btn-default hvr-sweep-to-bottom" data-toggle="collapse" data-target="#moreprojects"><h4>See More</h4></button>');
   });
   $("#moreprojects").on("show.bs.collapse", function(){
-    $("#toggleproject").html('<h4>See Less</h4>');
+    $("#toggleproject").html('<button class="btn btn-default hvr-sweep-to-top" data-toggle="collapse" data-target="#moreprojects"><h4>See Less</h4></button>');
   });
 });
