619b53ae5d7dd7c15fa219bf0c6c8acdb9d43702
Dalton Craven <20dalton00@gmail.com>
2018-12-22 13:56:52 -0500

    fix fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,27 +5,27 @@ import styled from "styled-components"
 import "./layout.css"
 
 const Container = styled.div`
-  width: 100%;
-  max-width: 650px;
-  margin: 0 auto;
+width: 100%;
+max-width: 650px;
+margin: 0 auto;
 
 
 
-  @media (max-width: 600px) {
-    padding: 10px;
-  }
+@media (max-width: 600px) {
+  padding: 10px;
+}
 `
 
 export default ({ children }) => (
   <>
   <Helmet>
     <html lang="en" />
+    <meta charSet="utf-8" />
     <meta name="docsearch:version" content="2.0" />
     <meta
       name="viewport"
-      content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
-    />
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,700" rel="stylesheet" />
+      content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover" />
+    <title>Dalton Craven</title>
   </Helmet>
   <Header />
   <Container>
