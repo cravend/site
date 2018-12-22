828a7d8c3b35f77b1233fd6883a62a9aa30542b5
Dalton Craven <20dalton00@gmail.com>
2018-12-22 18:09:49 -0500

    use google analytics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
 export default () => (
   <div>
     <p style={{ float: 'left' }}>&copy; 2018 Dalton Craven</p>
     <p style={{ float: 'right' }}>
-      <a href="https://github.com/cravend/site">View source on GitHub</a>
+      <OutboundLink href="https://github.com/cravend/site">View source on GitHub</OutboundLink>
     </p>
   </div>
 )
