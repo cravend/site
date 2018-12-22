19ee6ebc2cd3b257179a5be65afd8e1660c3afbd
Dalton Craven <20dalton00@gmail.com>
2018-12-22 16:27:25 -0500

    change to PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: 'Dalton Craven',
+    title: 'Dalton Craven'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/images/dalton-craven.jpg',
+        injectHTML: true
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images`
       }
     },
     {
@@ -18,12 +25,24 @@ module.exports = {
           {
             family: `Poppins`,
             variants: [`300`, `700`]
-          },
-        ],
-      },
+          }
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`
-  ],
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Dalton Craven`,
+        short_name: `Dalton Craven`,
+        start_url: `/`,
+        background_color: `#a8dcd9`,
+        theme_color: `#000000`,
+        icon: `src/images/dalton-craven.jpg`
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -21,9 +21,14 @@ export default ({ children }) => (
       <meta charSet="utf-8" />
       <meta name="docsearch:version" content="2.0" />
       <meta
+        name="Description"
+        content="The digital home of Dalton Craven, high school student and up-and-coming developer."
+      />
+      <meta
         name="viewport"
         content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
       />
+      <link rel="canonical" href="https://www.daltoncraven.me" />
       <title>Dalton Craven</title>
     </Helmet>
     <Header />
