82c06fdb0080db2c5c4e33ce293fd7840b2e7746
Dalton Craven <20dalton00@gmail.com>
2019-07-03 17:45:18 -0400

    update files/restart time

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Layout from '../components/layout'
 
 import About from '../components/About'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 
 class Index extends React.Component {
@@ -11,8 +9,6 @@ class Index extends React.Component {
     return (
       <Layout>
         <About />
-        <hr />
-        <Contact />
         <hr />
         <Footer />
       </Layout>
