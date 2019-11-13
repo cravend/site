b167a72055e1e521a9f4bfaddbb3450742e053ad
Dalton Craven <20dalton00@gmail.com>
2019-11-13 17:56:34 -0500

    add dark mode

---
+++ src/components/Theme.js
@@ -0,0 +1,16 @@
+export const lightTheme = {
+  breakpoint: '768px',
+  colors: {
+    primary: '#363534',
+    inverse: '#fbfffe',
+    accent: '#a8dcd9'
+  }
+}
+export const darkTheme = {
+  breakpoint: '768px',
+  colors: {
+    primary: '#FAFAFA',
+    inverse: '#363537',
+    accent: '#FAFAFA'
+  }
+}
