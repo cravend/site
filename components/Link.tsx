a283fa313e37955247ea43b6ec283c1f67d4ab53
Dalton <20dalton00@gmail.com>
2021-03-21 16:18:09 +0100

    add sample translation

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,23 +1,35 @@
 import React, { FC, ReactNode } from 'react';
 import NextLink from 'next/link';
 import { Link as StyledLink } from 'theme-ui';
+import { Locale } from '../src/translations/types';
 
 type LinkType = {
   to: string,
   isExternal?: boolean,
+  locale?: Locale,
   children: ReactNode
 }
 
-const Link: FC<LinkType> = ({ to, isExternal, children }: LinkType) => {
+const Link: FC<LinkType> = ({
+  to, isExternal, locale, children,
+}: LinkType) => {
   const externalCheck = /^https?:\/\//;
   if (isExternal || externalCheck.test(to)) {
     return <StyledLink href={to}>{children}</StyledLink>;
   }
-  return <NextLink passHref href={to}><StyledLink>{children}</StyledLink></NextLink>;
+  return (
+    <NextLink
+      passHref
+      locale={locale}
+      href={to}
+    ><StyledLink>{children}</StyledLink>
+    </NextLink>
+  );
 };
 
 Link.defaultProps = {
   isExternal: false,
+  locale: undefined,
 };
 
 export default Link;
