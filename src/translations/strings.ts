c59c02bd402d03feb25c15077372d726f6a6b76d
Dalton <20dalton00@gmail.com>
2021-03-21 16:17:30 +0100

    add locale config

---
+++ src/translations/strings.ts
@@ -0,0 +1,16 @@
+import { Strings } from './types';
+
+const en = {
+  tagline: 'student & developer',
+  test1: 'test',
+};
+
+const fr = {
+  tagline: 'Étudiant',
+  test2: 'test',
+};
+
+export default {
+  en,
+  fr,
+} as Strings;
