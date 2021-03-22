9c168274879fcd6f6b08183a077931a5c472117e
Dalton <20dalton00@gmail.com>
2021-03-22 11:40:28 +0100

    translate header component

--- src/translations/strings.ts
+++ src/translations/strings.ts
@@ -1,11 +1,19 @@
 import { Strings } from './types';
 
+const common = {
+  name: 'Dalton Craven',
+};
+
 const en = {
+  ...common,
   tagline: 'Student & developer',
+  pfp: 'Profile picture',
 };
 
 const fr = {
+  ...common,
   tagline: 'Étudiant & développeur',
+  pfp: 'Image de profil',
 };
 
 export default {
