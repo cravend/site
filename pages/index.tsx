31ee97d9e668376a4322ee3bd8982ec9e521690f
Dalton <20dalton00@gmail.com>
2021-03-22 14:31:41 +0100

    translate main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,71 +8,80 @@ import Header from '../components/Header';
 import Link from '../components/Link';
 import Feature from '../components/Feature';
 import Footer from '../components/Footer';
+import useLocalization from '../src/hooks/useLocalization';
 
-const IndexPage: FC = () => (
-  <Grid variant="layout">
-    <Head title="Home" />
-    <Header />
-    <Container>
-      <Text>
-        <Text as="span" variant="headline" sx={{ display: 'block' }}>
-          Hello! My name is <Text as="span" variant="gradient">Dalton Craven</Text>.{' '}
+const IndexPage: FC = () => {
+  const [t] = useLocalization();
+  return (
+    <Grid variant="layout">
+      <Head title="Home" />
+      <Header />
+      <Container>
+        <Text>
+          <Text as="span" variant="headline" sx={{ display: 'block' }}>
+            {t('greeting')}{' '}
+            <Text as="span" variant="gradient">{t('name')}</Text>.{' '}
+          </Text>
+          {t('intro')}
         </Text>
-        It's hard to introduce myself over the internet, but here's a little bit about me:
-      </Text>
-      <Grid
-        gap={3}
-        columns={[1, 2]}
-        my={3}
-      >
-        <Feature heading="Education">
-          <Text as="p">I'm a sophomore at the University of South Carolina. I'm pursuing a Bachelor of Science in Computer Science with a major in computer science and a minor in psychology (weird, right?). I'm also a McNair Scholar as part of UofSC's most prestigious scholarship program, the University Top Scholars.</Text>
-        </Feature>
-        <Feature heading="Study Abroad">
-          <Text as="p">I'm currently studying abroad at the American University of Paris! I'll be in France for the entirety of 2021. Spending an extended period of time in a different culture has been a goal of mine for a long time, and I'm looking forward to fully experiencing it <em>when</em> COVID is finally over.</Text>
-        </Feature>
-        <Feature heading="Work">
-          <Text as="p">I recently wrapped up a full-time internship as a front-end web developer at <Link to="https://cloverleaf.me">Cloverleaf</Link>. There, I used a variety of technologies (primarily Next/React and GraphQL) in an agile development team to routinely publish production code for our application. I was also an undergraduate teaching assistant for the spring 2020 semester, running eight sections of Linux/UNIX labs for approximately 250 students.</Text>
-        </Feature>
-        <Feature heading="Freelancing">
-          <Text as="p">I also do freelance web development! I work primarily with small businesses (although I’m open for anything), with an emphasis on providing resources and training to ensure long-term functionality.</Text>
-          <ul>
-            <li><Link to="https://crystalclearcontractors.com">Crystal Clear Cleaning &amp; Contracting</Link></li>
-            <li><Link to="https://elitepaintcincy.com">Elite Painting</Link></li>
-            <li><Link to="https://elitecontractingcincy.com">Elite Contracting</Link></li>
-            <li><Link to="https://mabbaseball.com">Mason Amateur Baseball</Link></li>
-          </ul>
-        </Feature>
-      </Grid>
-      <Box>
-        <Heading as="h2">Skills &amp; Certifications</Heading>
-        <ul>
-          <li>
-            <strong>Languages:</strong> React, GraphQL, Java, HTML, CSS, JavaScript, Visual Basic .NET, C++, PHP, SQL, Python, R
-          </li>
-          <li>
-            <strong>Tools:</strong> Next, Git, GitHub, GitLab, Gatsby, cPanel, WHM, Flask, Django, Linux, DigitalOcean, AWS, Netlify, WordPress
-          </li>
-          <li>
-            <strong>Certifications:</strong>
+        <Grid
+          gap={3}
+          columns={[1, 2]}
+          my={3}
+        >
+          <Feature heading={t('education_header')}>
+            <Text as="p">{t('education')}</Text>
+          </Feature>
+          <Feature heading={t('sa_header')}>
+            <Text as="p">{t('study_abroad')}</Text>
+          </Feature>
+          <Feature heading={t('work_header')}>
+            <Text as="p">
+              {t('work_1')}{' '}
+              <Link to="https://cloverleaf.me">{t('cloverleaf')}</Link>.{' '}
+              {t('work_2')}
+            </Text>
+          </Feature>
+          <Feature heading={t('freelance_header')}>
+            <Text as="p">{t('freelance')}</Text>
             <ul>
-              <li>The Fundamentals of Digital Marketing, Google — <em>Certified April 2020</em></li>
-              <li>Microsoft Office Specialist (MOS) Master 2016, MOS Word 2016 Expert, MOS Excel 2016 Expert, MOS PowerPoint 2016, MOS Outlook 2016, MOS Access 2016 — <em>Certified Fall 2018</em></li>
+              <li><Link to="https://crystalclearcontractors.com">Crystal Clear Cleaning &amp; Contracting</Link></li>
+              <li><Link to="https://elitepaintcincy.com">Elite Painting</Link></li>
+              <li><Link to="https://elitecontractingcincy.com">Elite Contracting</Link></li>
+              <li><Link to="https://mabbaseball.com">Mason Amateur Baseball</Link></li>
             </ul>
-          </li>
-        </ul>
-      </Box>
-      <Box>
-        <Heading as="h2">Contact</Heading>
-        <ul>
-          <li><strong>American Phone:</strong> <Link isExternal to="tel:+15138136421">+1 513 813 6421</Link></li>
-          <li><strong>French Phone:</strong> <Link isExternal to="tel:+330749792094">+33 07 49 79 20 94</Link></li>
-          <li><strong>Email:</strong> <Link isExternal to="mailto:daltonjcraven@gmail.com">daltonjcraven@gmail.com</Link></li>
-        </ul>
-      </Box>
-    </Container>
-    <Footer />
-  </Grid>
-);
+          </Feature>
+        </Grid>
+        <Box>
+          <Heading as="h2">{t('skills')}</Heading>
+          <ul>
+            <li>
+              <strong>{t('languages')}</strong> React, GraphQL, Java, HTML, CSS, JavaScript, Visual Basic .NET, C++, PHP, SQL, Python, R
+            </li>
+            <li>
+              <strong>{t('tools')}</strong> Next, Git, GitHub, GitLab, Gatsby, cPanel, WHM, Flask, Django, Linux, DigitalOcean, AWS, Netlify, WordPress
+            </li>
+            <li>
+              <strong>{t('certifications')}</strong>
+              <ul>
+                <li>The Fundamentals of Digital Marketing, Google — <em>{t('cert_april_2020')}</em></li>
+                <li>Microsoft Office Specialist (MOS) Master 2016, MOS Word 2016 Expert, MOS Excel 2016 Expert, MOS PowerPoint 2016, MOS Outlook 2016, MOS Access 2016 — <em>{t('cert_fall_2018')}</em></li>
+              </ul>
+            </li>
+          </ul>
+        </Box>
+        <Box>
+          <Heading as="h2">{t('contact')}</Heading>
+          <ul>
+            <li><strong>{t('american_phone')}</strong> <Link isExternal to="tel:+15138136421">+1 513 813 6421</Link></li>
+            <li><strong>{t('french_phone')}</strong> <Link isExternal to="tel:+330749792094">+33 07 49 79 20 94</Link></li>
+            <li><strong>{t('email')}</strong> <Link isExternal to="mailto:daltonjcraven@gmail.com">daltonjcraven@gmail.com</Link></li>
+          </ul>
+        </Box>
+      </Container>
+      <Footer />
+    </Grid>
+  );
+};
 
 export default IndexPage;
--- src/translations/strings.ts
+++ src/translations/strings.ts
@@ -3,6 +3,7 @@ import { Strings } from './types';
 const common = {
   name: 'Dalton Craven',
   github: 'GitHub',
+  cloverleaf: 'Cloverleaf',
 };
 
 const en = {
@@ -10,6 +11,27 @@ const en = {
   tagline: 'Student & Developer',
   pfp: 'Profile picture',
   viewSource: 'View source on',
+  greeting: 'Hello! My name is',
+  intro: 'It\'s hard to introduce myself over the internet, but here\'s a little bit about me:',
+  education_header: 'Education',
+  education: 'I\'m a sophomore at the University of South Carolina. I\'m pursuing a Bachelor of Science in Computer Science with a major in computer science and a minor in psychology (weird, right?). I\'m also a McNair Scholar as part of UofSC\'s most prestigious scholarship program, the University Top Scholars.',
+  sa_header: 'Study Abroad',
+  study_abroad: 'I\'m currently studying abroad at the American University of Paris! I\'ll be in France for the entirety of 2021. Spending an extended period of time in a different culture has been a goal of mine for a long time, and I\'m looking forward to fully experiencing it when COVID is finally over.',
+  work_header: 'Work',
+  work_1: 'I recently wrapped up a full-time internship as a front-end web developer at',
+  work_2: 'There, I used a variety of technologies (primarily Next/React and GraphQL) in an agile development team to routinely publish production code for our application. I was also an undergraduate teaching assistant for the spring 2020 semester, running eight sections of Linux/UNIX labs for approximately 250 students.',
+  freelance_header: 'Freelancing',
+  freelance: 'I also do freelance web development! I work primarily with small businesses (although I’m open for anything), with an emphasis on providing resources and training to ensure long-term functionality.',
+  skills: 'Skills & Certifications',
+  languages: 'Languages:',
+  tools: 'Tools:',
+  certifications: 'Certifications:',
+  cert_april_2020: 'Certified April 2020',
+  cert_fall_2018: 'Certified fall 2018',
+  contact: 'Contact',
+  american_number: 'American phone:',
+  french_number: 'French phone:',
+  email: 'Email:',
 };
 
 const fr = {
@@ -17,6 +39,28 @@ const fr = {
   tagline: 'Étudiant & Développeur',
   pfp: 'Image de profil',
   viewSource: 'Voir le code source sur',
+  greeting: 'Salut ! Je m\'appelle',
+  intro: 'Il est difficile de me présenter sur Internet, mais voici un peu de moi :',
+  education_header: 'Éducation',
+  education: 'Je suis étudiant en deuxième année à l’Université de Caroline du Sud. Je poursuis un baccalauréat ès sciences en informatique avec une mineure en psychologie. Je suis également boursier McNair dans le cadre du programme de bourses le plus prestigieux, les « University Top Scholars ».',
+  sa_header: 'Étudier à L’étranger',
+  study_abroad: 'J’étudie actuellement à l’étranger à l’Université américaine de Paris ! Je serai en France pendant toute l’année 2021. Passer une longue période de temps dans une culture différente est un de mes objectifs depuis longtemps, et j’ai hâte de vivre pleinement lorsque COVID sera enfin terminé.',
+  work_header: 'Travailler',
+  work_1: 'J’ai récemment terminé un stage à temps plein en tant que développeur web front-end chez',
+  work_2: 'Là, j’ai utilisé une variété de technologies (principalement Next / React et GraphQL) dans une équipe de développement agile pour publier régulièrement du code de production pour notre application. J\'étais également assistant d\'enseignement de premier cycle pour le semestre de printemps 2020, dirigeant huit sections de cours Linux / UNIX pour environ 250 étudiants.',
+  freelance_header: 'Freelance',
+  freelance: 'Je fais aussi du développement web en freelance ! Je travaille principalement avec des petites entreprises (bien que je sois ouvert à tout), en mettant l’accent sur la fourniture de ressources et de formation pour assurer une fonctionnalité à long terme.',
+  skills: 'Compétences & Certifications',
+  languages: 'Langues :',
+  tools: 'Outils :',
+  certifications: 'Certifications :',
+  cert_april_2020: 'Certifié avril 2020',
+  cert_fall_2018: 'Certifié automne 2018',
+  contact: 'Contact',
+  american_phone: 'Numéro américain :',
+  french_phone: 'Numéro français :',
+  email: 'Email :',
+
 };
 
 export default {
