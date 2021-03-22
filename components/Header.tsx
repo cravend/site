fa6c4f3ba3e2c17b4ca07bda83f60741b04836b1
Dalton <20dalton00@gmail.com>
2021-03-22 11:40:28 +0100

    translate header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,47 +2,48 @@ import React, { FC } from 'react';
 import {
   Box, Image, Heading, Container,
 } from 'theme-ui';
+import useLocalization from '../src/hooks/useLocalization';
 import Hero from './Hero';
 
-const Header: FC = () => (
-  <Hero isDiagonal>
-    <Container
-      variant="copy"
-      m="0 auto"
-      p={3}
-      sx={{
-        display: 'flex',
-        height: '100%',
-        flexDirection: ['column', 'row'],
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
-      <Image
-        src="/images/dalton-craven.jpg"
-        alt="pfp"
-        width={150}
-        sx={{ borderRadius: '50%' }}
-      />
-      <Box
-        ml={[0, 20]}
-        p={0}
+const Header: FC = () => {
+  const [t] = useLocalization();
+
+  return (
+    <Hero isDiagonal>
+      <Container
+        variant="copy"
+        m="0 auto"
+        p={3}
         sx={{
-          marginLeft: [0, 2],
-          marginTop: [2, 0],
-          textAlign: ['center', 'right'],
-          color: 'white',
+          display: 'flex',
+          height: '100%',
+          flexDirection: ['column', 'row'],
+          alignItems: 'center',
+          justifyContent: 'space-around',
         }}
       >
-        <Heading as="h1" variant="title">
-          Dalton Craven
-        </Heading>
-        <Heading as="h3" variant="subtitle" sx={{ textTransform: 'lowercase' }}>
-          Student &amp; Developer
-        </Heading>
-      </Box>
-    </Container>
-  </Hero>
-);
+        <Image
+          src="/images/dalton-craven.jpg"
+          alt={t('pfp')}
+          width={150}
+          sx={{ borderRadius: '50%' }}
+        />
+        <Box
+          ml={[0, 20]}
+          p={0}
+          sx={{
+            marginLeft: [0, 2],
+            marginTop: [2, 0],
+            textAlign: ['center', 'right'],
+            color: 'white',
+          }}
+        >
+          <Heading as="h1" variant="title">{t('name')}</Heading>
+          <Heading as="h3" variant="subtitle" sx={{ textTransform: 'lowercase' }}>{t('tagline')}</Heading>
+        </Box>
+      </Container>
+    </Hero>
+  );
+};
 
 export default Header;
