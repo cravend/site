3dfd56eb84f87ed86cb47b33773467ea71ce8344
Dalton <20dalton00@gmail.com>
2021-03-25 00:51:39 +0100

    remove matomo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,10 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { ThemeProvider } from 'theme-ui';
 import { AppProps } from 'next/app';
 import theme from '@cravend/theme';
-import { init } from '@socialgouv/matomo-next';
 import { withAutomaticColorMode } from '../src/hooks/useColorMode';
 
 const App:FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    const url = process.env.NEXT_PUBLIC_MATOMO_URL;
-    const siteId = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
-    if (!url || !siteId) {
-      return;
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    init({ url, siteId });
-  }, []);
-
   const ComponentWithColorMode = withAutomaticColorMode(Component);
 
   return (
