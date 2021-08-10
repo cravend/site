0d6bcbf119898fd52aebedd834594b113ddbfda6
Dalton Craven <20dalton00@gmail.com>
2021-08-10 03:13:35 +0200

    Write tests for all pure-TS files

---
+++ src/translations/__tests__/util.test.ts
@@ -0,0 +1,51 @@
+import { NextRouter } from "next/router";
+import { toggleLocale, isLocale, getLocale } from "../util";
+
+describe("isLocale()", () => {
+  it("returns true for valid locales", () => {
+    expect.assertions(2);
+    expect(isLocale("en")).toBeTruthy();
+    expect(isLocale("fr")).toBeTruthy();
+  });
+
+  it("returns false for invalid locales", () => {
+    expect.assertions(2);
+    expect(isLocale("x")).toBeFalsy();
+    expect(isLocale(null)).toBeFalsy();
+  });
+});
+
+describe("getLocale()", () => {
+  it("returns the router's locale if defined", () => {
+    expect.assertions(1);
+    const mockRouter = { locale: "en", defaultLocale: "fr" } as NextRouter;
+    expect(getLocale(mockRouter)).toBe("en");
+  });
+
+  it("returns the router's defaultLocale is locale is undefined", () => {
+    expect.assertions(1);
+    const mockRouter = { locale: undefined, defaultLocale: "fr" } as NextRouter;
+    expect(getLocale(mockRouter)).toBe("fr");
+  });
+
+  it("returns the config's defaultLocale if the router's defaultLocale is undefined", () => {
+    expect.assertions(1);
+    const mockRouter = {
+      locale: undefined,
+      defaultLocale: undefined,
+    } as NextRouter;
+    expect(getLocale(mockRouter)).toBe("en");
+  });
+});
+
+describe("toggleLocale()", () => {
+  it("toggles from English to French", () => {
+    expect.assertions(1);
+    expect(toggleLocale("en")).toBe("fr");
+  });
+
+  it("toggles from French to English", () => {
+    expect.assertions(1);
+    expect(toggleLocale("fr")).toBe("en");
+  });
+});
