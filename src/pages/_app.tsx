5a2d74c046b1a6bdd059ddab495fa6cb14f7c0df
Dalton Craven <20dalton00@gmail.com>
2021-08-25 01:55:13 +0200

    build(deps): remove theme ui and @cravend/theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,5 @@
 import { useEffect } from "react";
-import { ThemeProvider } from "theme-ui";
 import { init } from "@socialgouv/matomo-next";
-import theme from "@cravend/theme";
-import type { Theme } from "theme-ui";
 import type { AppProps } from "next/app";
 import type { FC } from "react";
 import "../styles/global.scss";
@@ -17,10 +14,8 @@ const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   }, []);
 
   return (
-    <ThemeProvider theme={theme as Theme}>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </ThemeProvider>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Component {...pageProps} />
   );
 };
 
