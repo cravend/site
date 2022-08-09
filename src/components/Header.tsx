24e2d7e3a6f21881604cf97e621f71864e9697d8
Dalton Craven <daltoncraven@proton.me>
2022-08-09 23:29:19 +0200

    feat: convert components to next-intl syntax

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 import profilePic from "../assets/images/dalton-craven.webp";
-import useLocalization from "../i18n/useLocalization";
 import styles from "../styles/modules/Header.module.scss";
 
 import Hero from "./Hero";
 
 const Header = () => {
-  const [t] = useLocalization();
+  const t = useTranslations("Header");
 
   return (
     <Hero isDiagonal>
@@ -22,9 +22,9 @@ const Header = () => {
           />
         </div>
         <div className={styles.text_container}>
-          <h1 className={styles.title}>{t("name")}</h1>
+          <h1 className={styles.title}>{t.rich("name")}</h1>
           <p className={styles.subtitle}>
-            <b>{t("tagline")}</b>
+            <b>{t.rich("tagline")}</b>
           </p>
         </div>
       </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,25 @@
+import { useRtl } from "../i18n/hooks";
 import styles from "../styles/modules/Hero.module.scss";
 
 export type HeroProps = {
-  isDiagonal?: boolean;
   children: React.ReactNode;
+  isDiagonal?: boolean;
+};
+
+const getClassNames = (isDiagonal: boolean, isRtl: boolean) => {
+  const classNames = [];
+  if (isDiagonal) {
+    classNames.push(styles.diagonal);
+  } else {
+    classNames.push(styles.normal);
+  }
+  if (isRtl) {
+    classNames.push(styles.rtl);
+  }
+  return classNames.length <= 1 ? classNames[0] : classNames.join(" ");
 };
 
 const Hero = ({ isDiagonal = false, children }: HeroProps) => (
-  <header className={isDiagonal ? styles.diagonal : styles.normal}>
-    {children}
-  </header>
+  <header className={getClassNames(isDiagonal, useRtl())}>{children}</header>
 );
 export default Hero;
