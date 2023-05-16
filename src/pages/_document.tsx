d0c14e3edad2157cf82013347cc5e2c66cc6ce87
Dalton Craven <daltoncraven@proton.me>
2023-05-16 01:47:35 -0400

    feat: switch to more accessible font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,7 +46,7 @@ const Document = ({ locale, ...props }: DocumentProps) => {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,700;1,300;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
         <meta
