0bb4ca99fc01763c40f7b23dff44c703d348eb08
Dalton Craven <daltoncraven@proton.me>
2023-05-24 02:02:41 -0400

    chore: install Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-import i18n from "astro-i18n"
+import i18n from "astro-i18n";
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({integrations: [i18n()],});
+export default defineConfig({
+  integrations: [i18n(), tailwind()]
+});
