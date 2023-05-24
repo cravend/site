6557b43c329930e7632601d55ea7d2a35e61aaa4
Dalton Craven <daltoncraven@proton.me>
2023-05-24 03:20:28 -0400

    feat: configure type-checking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import i18n from "astro-i18n";
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
