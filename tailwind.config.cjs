bbed96d023cb3f70244710272166532e961bff6c
Dalton Craven <daltoncraven@proton.me>
2023-05-24 02:51:19 -0400

    feat: install Atkinson Hyperlegible

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["'Atkinson Hyperlegible'", ...defaultTheme.fontFamily.sans],
+
+			}
+		},
 	},
 	plugins: [],
 }
