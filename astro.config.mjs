9af7518a3686685104c5f1162688a8a0ef07f271
Dalton Craven <daltoncraven@proton.me>
2023-05-28 13:15:49 -0400

    feat: enable Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import i18n from "astro-i18n";
 import { defineConfig } from 'astro/config';
 
-// import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18n(),]
-  // integrations: [i18n(), tailwind()]
+  integrations: [i18n(), tailwind()]
 });
