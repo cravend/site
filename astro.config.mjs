76fd919a9ccbace3ac0964a71810ea8dc46f3fdd
Dalton Craven <daltoncraven@proton.me>
2023-06-25 16:44:27 +0200

    refactor: apply underline to all links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import i18n from "astro-i18n";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18n(), tailwind()],
+  integrations: [
+    i18n(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   experimental: {
-    assets: true
-  }
+    assets: true,
+  },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,13 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["'Atkinson Hyperlegible'", ...defaultTheme.fontFamily.sans],
-
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Atkinson Hyperlegible'", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
