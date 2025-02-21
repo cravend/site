9e154df49d5196834ee83358c759b3ae54634fc6
Dalton Craven <dalton.craven@qantev.com>
2025-02-21 09:10:49 +0100

    chore: configure prettier and eslint

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
---
+++ eslint.config.ts
@@ -0,0 +1,15 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  ...tseslint.configs.strict,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  { rules: { "arrow-body-style": ["error", "as-needed"] } },
+  eslintConfigPrettier,
+);
